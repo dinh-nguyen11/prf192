6617f36fd690bcfd5b65a5d88df0f8df30069624
dinh-nguyen11 <caothimai1511@gmail.com>
2026-01-21 09:35:43 +0700

    Slot 5

--- Slot 5/ex4.c
+++ Slot 5/ex4.c
@@ -4,20 +4,23 @@ int main (){
 	printf("N : ");
 	scanf("%d",&N);
 	if (N<=100 && N>0){
-		t=950;
-		
+		T=950;
+		t=N*950;
 		//printf("so tien :%d",t);
 	}
 	else if ( N>100 && N<=150){
-		t=1250;
+		T=1250;
+		t=1250*(N-100)+950*100;
 		//printf("so tien :%d",t);
 	}
 	    else if (N>150 && N<=200){
-	    	t=1350;
+	    	T=1350;
+	    	t=100*950+50*1250+(N-150)*1350;
 	    //	printf("so tien :%d",t);
 		}
 		   else {
-		   t = 1550;
+		   T = 1550;
+		   t=100*950+50*1250+50*1350+(N-200)*1550;
 		   //printf("so tien :%d",t);
 		   }
  printf("so tien :%d",t);
