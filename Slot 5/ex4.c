e8cbeb3ab2948908e661a866ece20c7e763fd636
dinh-nguyen11 <caothimai1511@gmail.com>
2026-01-21 09:15:16 +0700

    Slot 5

---
+++ Slot 5/ex4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main (){
+	int N, t, T;
+	printf("N : ");
+	scanf("%d",&N);
+	if (N<=100 && N>0){
+		t=950;
+		
+		//printf("so tien :%d",t);
+	}
+	else if ( N>100 && N<=150){
+		t=1250;
+		//printf("so tien :%d",t);
+	}
+	    else if (N>150 && N<=200){
+	    	t=1350;
+	    //	printf("so tien :%d",t);
+		}
+		   else {
+		   t = 1550;
+		   //printf("so tien :%d",t);
+		   }
+		   T=t*N;
+ printf("so tien :%d",T);
+ return 0;
+}
