66ac5d22e30d6024bc5978031d34f65e4ff27f47
dinh-nguyen11 <caothimai1511@gmail.com>
2026-01-28 08:58:40 +0700

    Workshop

--- Workshop/workshop1/ex6.c
+++ Workshop/workshop1/ex6.c
@@ -1,13 +1,12 @@
-#include<stdio.h>;
+#include<stdio.h>
 int main (){
 	float cc,gk,ck,T;
 	char diemchu;
 	printf("nhap diem chuyen can, giua ki, cuoi ki :");
-	scanf("%f %f %f",&cc,&gk,&ck);
 	if(scanf("%f %f %f",&cc,&gk,&ck)!=3 || cc<0 || gk<0 || ck<0 ){
 		printf("Nhap sai!");
 		return 0;
-	} else T=cc*0.1+gk*0.3+ck*0.6;
+	 }else T=cc*0.1+gk*0.3+ck*0.6;
 	  if (T>=8.5 && T<=10){
 		  diemchu='A';
 	    }else if (T<8.5 && T>=7){
@@ -18,7 +17,7 @@ int main (){
 	     	diemchu='D';
 	         }else diemchu='F';
 	if (cc>=4 && gk>=4 && ck>=4 && diemchu !='F'){
-		printf("DAT!");
-	     }else printf("Khong DAT!");
+		printf("BAN DU DIEU KIEN TOT NGHIEP!");
+	     }else printf("BAN KHONG DU DIEU KIEN TOT NGHIEP!");
 return 0;
 }
