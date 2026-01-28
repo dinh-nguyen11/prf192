0bcb011e29ba63009b124558af6503a6aacaf7fc
dinh-nguyen11 <caothimai1511@gmail.com>
2026-01-28 08:05:25 +0700

    Workshop

--- Workshop/workshop1/ex6.c
+++ Workshop/workshop1/ex6.c
@@ -1,20 +1,24 @@
 #include<stdio.h>;
 int main (){
 	float cc,gk,ck,T;
-	char chu;
-	scanf("%f %f %f",&cc, &gk, &ck);
-	T=cc*0.1+gk*0.3+ck*0.6;
-	if (T>=8){
-		chu='A';
-	 }else if (T<8 && T>=6){
-		chu='B';
-	  } else if (T<6 && T>=4){
-		chu='C';
-	     }else if (T<4 && T>=2){
-		chu='D';
-	      }else chu='F';
-	if (cc,gk,ck>=4 && chu !='F'){
+	char diemchu;
+	printf("nhap diem chuyen can, giua ki, cuoi ki :");
+	scanf("%f %f %f",&cc,&gk,&ck);
+	if(scanf("%f %f %f",&cc,&gk,&ck)!=3 || cc<0 || gk<0 || ck<0 ){
+		printf("Nhap sai!");
+		return 0;
+	} else T=cc*0.1+gk*0.3+ck*0.6;
+	  if (T>=8.5 && T<=10){
+		  diemchu='A';
+	    }else if (T<8.5 && T>=7){
+	    	diemchu='B';
+	     } else if (T<7 && T>=5.5){
+	    	diemchu='C';
+	       }else if (T<5.5 && T>4){
+	     	diemchu='D';
+	         }else diemchu='F';
+	if (cc>=4 && gk>=4 && ck>=4 && diemchu !='F'){
 		printf("DAT!");
-	}else printf("Khong DAT!");
+	     }else printf("Khong DAT!");
 return 0;
 }
